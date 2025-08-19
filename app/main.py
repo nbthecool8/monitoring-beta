31f98fa294a5e09a72fa23dd1d29af42f762564a
root <root@ZEUS.localdomain>
2025-08-19 04:34:32 +0100

    demo: change response text

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class H(BaseHTTPRequestHandler):
+    def do_GET(self):
+        msg = b"hello from CI/CD v2\n"
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(msg)
+
+HTTPServer(('', 8080), H).serve_forever()
